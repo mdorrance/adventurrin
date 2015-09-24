c708b172f88595d442da77544331a1d212a5b884
Michael Dorrance <dorrancemd@gmail.com>
2015-09-24 15:05:12 -0600

    added map to user#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get  '/auth/instagram/callback', to: 'sessions#create'
   resources :users, only: [:index, :show]
+  get '/logout',                   to: 'sessions#destroy'
   root 'static_pages#index'
 
 
