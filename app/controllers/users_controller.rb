fc6d0c0fa5079dc1d1120fdfc0cb58d609f04637
Michael Dorrance <dorrancemd@gmail.com>
2015-09-26 15:59:15 -0600

    completed location search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
   def index
     @feed = current_user.client.user_media_feed
   end
+
+  def location_search
+    if params[:location] == ""
+      redirect_to users_path
+    else
+      lat_long = Geocoder.coordinates(params[:location])
+      location = current_user.client.location_search(lat_long[0].to_s,lat_long[1].to_s)
+      location_media = current_user.client.media_search(lat_long[0].to_s,lat_long[1].to_s)
+
+      if location_media[0].nil?
+      redirect_to users_path
+      else
+        @feed = location_media
+        @location = params[:location]
+      end
+    end
+  end
 end
