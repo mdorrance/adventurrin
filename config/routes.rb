62a179205da8021bcd1b12d48eb07efb36b9d0db
Michael Dorrance <dorrancemd@gmail.com>
2015-09-30 14:01:08 -0600

    rename routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get  '/auth/instagram/callback', to: 'sessions#create'
-  resources :users, only: [:index, :show]
+  get 'feed',                      to: 'users#index'
+  get 'profile',                   to: 'users#show'
   resources :adventures, only: [:create, :destroy]
-  get '/location_search',          to: 'users#location_search'
+  get '/locations',                to: 'users#location_search'
   get '/logout',                   to: 'sessions#destroy'
   root 'static_pages#index'
 
