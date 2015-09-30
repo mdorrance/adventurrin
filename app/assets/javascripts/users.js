28427cbf7b5e7af03897d05a06788c9f1c8fc510
Michael Dorrance <dorrancemd@gmail.com>
2015-09-30 13:11:29 -0600

    added modals for page redirects

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
   $(".feed-image").on("click", function(event){
     modalId = $(event.currentTarget).data("target")
-    
+    $(modalId).modal("show");
+  })
+
+  $("#location-search").submit(function(event){
+    modalId = $(event.currentTarget).data("data")
     $(modalId).modal("show")
+    $("#pending-search").modal("show");
+  })
+
+  $("#update-feed").click(function(event){
+    modalId = $(event.currentTarget).data("data")
+    $(modalId).modal("show")
+    $("#pending-feed-update").modal("show");
   })
 })
