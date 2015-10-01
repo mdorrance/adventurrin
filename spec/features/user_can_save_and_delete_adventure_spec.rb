44272fad2f130971f79ff587acf27cec8e8f43ba
Michael Dorrance <dorrancemd@gmail.com>
2015-10-01 08:50:13 -0600

    updated tests

--- spec/features/user_can_save_and_delete_adventure_spec.rb
+++ spec/features/user_can_save_and_delete_adventure_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "User can see feed", type: :feature do
+RSpec.describe "User can save and delete an adventure", type: :feature do
   context "A user with an instagram account" do
 
     it "renders feed" do
@@ -20,27 +20,7 @@ RSpec.describe "User can see feed", type: :feature do
         end
 
         visit feed_path
-        
-        within(".saved-adventure") do
-          expect(page).to have_content "Steamboat Springs, Colorado"
-        end
 
-      end
-    end
-
-    xit "renders feed without locations" do
-      VCR.use_cassette("user_who_has_no_friends_with_locations_sees_feed_with_locations_test#feed") do
-        visit root_path
-        login_user_no_location_feed
-
-        click_link "Login"
-
-        within(".navbar") do
-          expect(page).to have_content "adventurrus"
-        end
-        expect(page).to have_content "drone_365"
-        expect(page).to have_content "Cafecito"
-        expect(page).to have_content "+1-312-922-2233"
       end
     end
   end
